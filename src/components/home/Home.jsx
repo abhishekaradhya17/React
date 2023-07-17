fec100d6d81faf764484b7efaa189241b8c46640
Abhishek <abhishekaradhyamath17@outlook.com>
2023-07-17 10:53:42 +0530

    snooker

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import AboutCard from "../about/AboutCard"
+import Hblog from "./Hblog"
+import Hero from "./hero/Hero"
+import Hprice from "./Hprice"
+import Testimonal from "./testimonal/Testimonal"
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <AboutCard />
+      <Testimonal />
+      <Hblog />
+      <Hprice />
+    </>
+  )
+}
+
+export default Home
---
+++ src/dummydata.js
@@ -0,0 +1,124 @@
+export const homeAbout = [
+  {
+    id: 1,
+    cover: "https://img.icons8.com/dotty/80/000000/storytelling.png",
+    title: "Schedules",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/diploma.png",
+    title: "Lear with Friends",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/athlete.png",
+    title: "Learn with Expert",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+]
+export const awrapper = [
+  {
+    cover: "https://img.icons8.com/external-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto/80/ffffff/external-graduation-education-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto.png",
+    data: "300",
+    title: "SUCCESS STORIES",
+  },
+
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/athlete.png",
+    data: "320",
+    title: "CLUBS",
+  },
+  {
+    cover: "https://img.icons8.com/external-outline-icons-maxicons/80/ffffff/external-calender-insurance-outline-outline-icons-maxicons.png",
+    data: "1,000",
+    title: "SCHEDULES",
+  },
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/macbook-idea--v3.png",
+    data: "587",
+    title: "GROUPS",
+  },
+]
+
+
+
+export const price = [
+  {
+    name: "BASIC PLAN",
+    price: "49K",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+  {
+    name: "BEGINNER PLAN",
+    price: "79K",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+  {
+    name: "PREMIUM PLAN",
+    price: "109k",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+  {
+    name: "ULTIMATE PLAN",
+    price: "149K",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+  },
+]
+export const faq = [
+
+]
+export const blog = [
+  {
+    id: 1,
+    type: "admin",
+    date: "JAN. 18, 2021",
+    com: "3 COMMENTS ",
+    title: "Get a Gaming Experience",
+    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    cover: "../images/blog/b1.jpeg",
+  },
+  {
+    id: 2,
+    type: "admin",
+    date: "API. 25, 2022",
+    com: "5 COMMENTS ",
+    title: "Get a Gaming Experience",
+    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    cover: "../images/blog/b2.jpeg",
+  },
+  {
+    id: 3,
+    type: "user",
+    date: "MAY. 15, 2022",
+    com: "10 COMMENTS ",
+    title: "Get a Gaming Experience",
+    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    cover: "../images/blog/b3.jpg",
+  },
+
+]
+export const testimonal = [
+  {
+    id: 1,
+    name: "ROGER SCOTT",
+    post: "TRAIN EXPERT",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    cover: "./images/testo/t1.webp",
+  },
+  {
+    id: 2,
+    name: "ROGER SCOTT",
+    post: "MANAGER",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    cover: "./images/testo/t2.webp",
+  },
+  {
+    id: 3,
+    name: "ROGER SCOTT",
+    post: "DIRECTOR",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    cover: "./images/testo/t3.webp",
+  },
+]
